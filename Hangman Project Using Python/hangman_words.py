8a806c2ffb0097fb3f8823e8005fe9a9aa6a1a7f
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2025-09-22 22:35:30 +0600

    Add files via upload

---
+++ Hangman Project Using Python/hangman_words.py
@@ -0,0 +1,8 @@
+word_list = [
+    "bulb", "messi", "camel", "python", "hangman", "guitar", "pencil",
+    "computer", "keyboard", "monitor", "program", "algorithm", "notebook",
+    "laptop", "elephant", "tiger", "mountain", "river", "ocean", "butterfly",
+    "chocolate", "airplane", "bicycle", "coffee", "sunflower", "notebook",
+    "umbrella", "rainbow", "diamond", "jungle", "library", "backpack", "window",
+    "flower", "star", "planet", "soccer", "basketball", "football"
+]
---
+++ Hangman Project Using Python/main.py
@@ -0,0 +1,61 @@
+# Hangman Project
+
+import random
+import hangman_words
+import hangman_arts
+
+lives = 6
+print(hangman_arts.logo)
+
+chosen_word = random.choice(hangman_words.word_list)
+# print(chosen_word)
+
+word_length = len(chosen_word)
+placeholder = ""
+
+for position in range(word_length):
+    placeholder += "_"
+
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+    print(f"****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+    print(guess)
+
+    if guess in correct_letters:
+        print(f"You already guessed {guess}")
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        if lives == 0:
+            game_over = True
+            print(f"****************************IT WAS {chosen_word}! YOU LOSE****************************")
+
+    print(hangman_arts.stages[lives])
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+
+
